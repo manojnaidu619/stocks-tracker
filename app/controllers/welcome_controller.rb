e4efd1bff2a3d6c8fad29a29ddf09ba8d089069e
Manoj_Naidu619 <manojnaidu619@gmail.com>
2018-03-11 17:35:12 +0530

    Added associations

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,9 @@
 class WelcomeController < ApplicationController
+
   def index
     @user = User.all
+  end
+  def ajax
+    @ajax = User.create(comments: "like this shit!!")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'users/my_portfolio'
   get 'stocks/search_stocks'
+  get 'welcome/ajax'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
